520dfc8b53c106da6eb2d7dd4e7c66ac76a0456f
leonardo <leonardoorodrigues65@gmail.com>
2022-10-13 18:35:55 -0300

    final 2

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 
  
- alert("Gasolina Comum 6.80 litro ")
+//Exercicio 1
 
 
- let Dinheiro = prompt("Quanto em Dinheiro deseja abastecer")
+let Dinheiro = prompt("Quanto em Dinheiro deseja abastecer")
+let cliente = prompt("Nome do Cliente")
 let gasolina = 6.80;
 
 
 let qtdLitros =parseInt ( Dinheiro / gasolina );
 
-alert(`Foi abastecido em seu veiculo essa quantidade de Litros: ${qtdLitros.toFixed(1)}`)
+alert(`O ${cliente}  abasteceu essa quantidade de gasolina  ${qtdLitros} litros `)
+
+// Exercicio 2
 
 
-
-alert("Transforme graus Celsius em graus Fahrenheit.")
-
-
-let C = prompt("Informe a temperatura em graus Celsius")
+let C = prompt("Informe a temperatura em graus Celsius  para ser Transformada em Fahrenheit")
 
 let F = 0
 
@@ -25,23 +24,20 @@ let resultado = F = (9 * C+160) / 5
 alert(`temperatura em Fahrenheit ${resultado} `)
 
 
+//Exercicio 3
 
-
-alert("Transforme graus Fahrenheit em graus Celsius")
-
-let f = prompt("Informe a temperatura em Fahrenheit")
+let f = prompt("Informe a temperatura em Fahrenheit para ser Transformada em Celsius")
 
 let c = 0
 
 let resultado2 = c = ((f - 32)* 5) / 9;
 
-alert (`temperatura em graus Celsius  ${resultado2.toFixed(6)} graus`)
+alert (`temperatura em graus Celsius  ${resultado2.toFixed(5)} graus`)
 
 
+// Exercicio 4
 
-alert("Descubra o Comprimento , Largura e altura de uma Caixa retangular em metros")
-
-let com = prompt("Informe o Comprimento da Sua Caixa")
+let com = prompt("Descubra o Comprimento , Largura e altura de uma Caixa retangular em metros , PARA INICIAR  Informe o Comprimento da Sua Caixa:")           
 
 let lar = prompt("Informe a Largura da sua Caixa")
 
@@ -51,26 +47,24 @@ let volume = com*lar*alt;
 
 alert (`Resultado da sua Caixa ${volume} m³`)
 
+//Exercio 5
 
-
-alert("insira um valor numerico inteiro para saber o valor informado ao quadrado")
-
-let valor =prompt("Insira aqui")
+let valor =prompt("insira um valor numerico inteiro para saber o valor informado ao quadrado")
 
 let valorQuadrado = +valor*valor;
 
 alert(`O valor ${valor} ao quadrado é ${valorQuadrado}`)
 
 
-
+//Exercicio 6
 
 alert("Realize a soma de  6 numeros e tire a media")
 
-let nota1 = parseInt( prompt("insira o primeiro numero") )
+let nota1 = parseInt( prompt("Realize a soma de  6 numeros e tire a media :  Insira o Primeiro Numero :") )          
 let nota2 =parseInt( prompt("insira o segundo numero") )
-let nota3 = parseInt (  prompt("insira o terceiro numero") )
-let nota4 = parseInt ( prompt("insira o quarto numero") )
-let nota5 =  parseInt (  prompt("insira o quinto numero") )
+let nota3 = parseInt (prompt("insira o terceiro numero") )
+let nota4 = parseInt (prompt("insira o quarto numero") )
+let nota5 =  parseInt ( prompt("insira o quinto numero") )
 
 let soma = (nota1 + nota2 + nota3 + nota4 + nota5) 
 
@@ -79,12 +73,12 @@ let media = (soma/5)
 alert(`Resultado: ${soma} media: ${media}`)
 
 
-
+//Exercicio 7
 
 alert("Resto da divisão do Primeiro pelo Segundo")
 
-let valor1 = prompt("insira um numero")
-let valor2 = prompt ("insira um numero")
+let valor1 = prompt(" Resto da divisão do Primeiro pelo Segundo .Insira O Primeiro numero :")
+let valor2 = prompt ("insira o segundo numero")
 
 let resultadoSoma = ( valor1 * valor2 )
 
@@ -93,13 +87,13 @@ let Resto =( valor1 %  valor2 )
 alert(`Resultado da soma: ${resultadoSoma} resto:${Resto}`)
 
 
+// Exercicio 8
 
 
 
-alert("Escreva uma Frase ou uma palavra para saber a quantidade de caracteres")
 
 
-let frase = prompt("insira uma Palavra ou Frase")
+let frase = prompt("Escreva uma Frase ou uma palavra para saber a quantidade de caracteres")
 
 
 let qtdFrase = frase.length;
@@ -110,28 +104,29 @@ alert(`Essa é a quantidade de caracteres que sua frase ou palavra tem:${qtdFras
 
 
 
+//Exercicio 9
+
+let frase1 = prompt ("Primeira letra e ultima letra: insira uma palavra ou frase  ")
+
+let letra1 = frase1.toLocaleUpperCase()[0]
+
+let letra2 = frase1.substr(-1)
+
+alert(`primeira letra ${letra1} ultima letra ${letra2}    `)
 
 
-alert("Primeira letra e ultima letra")
-
-let inicio = prompt ("insira sua palavra ")
-
-let letra1 = letra1[0].tolocaluppercase() +letra1.substring(1)
+//Exercicio 10
 
 
-
-alert(`primeira letra ${letra1}`)
-
-
-
-
-alert("Informação Cracha")
-
-
-let nome = prompt("insira seu nome ")
+let nome = prompt("INFORMAÇÃO PARA SEU CRACHÁ:  Insira seu nome ")
 let sobrenome = prompt("informe seu sobrenome")
 let idade = prompt ("informe sua idade ")
 let setor = prompt ("informe seu setor")
 
+let resultadoNome = nome.toLocaleUpperCase()
+let resultadoSobre =sobrenome.toLocaleUpperCase()
 
-alert(`nome; ${nome} Sobrenome ${sobrenome} Idade: ${idade} Setor: ${setor}`)
+
+
+
+alert(`nome: ${resultadoNome}${resultadoSobre}    Idade - ${idade}   Setor- ${setor}`)
